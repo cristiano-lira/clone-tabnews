a96443d5f9a0b871ac510ca6bd5219c8951ae0d7
Cristiano Lira <cristiano_sclira@hotmail.com>
2025-07-04 02:01:18 +0000

    Correção do arquivo index.js (faltou o export default Home;)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-export default function Home() {
-  return <h1>Primeiro passo rumo aos 15k 😎</h1>;
+// export default function Home() {
+//   return <h1>Primeiro passo rumo aos 15k 😎</h1>;
+// }
+
+function Home() {
+    return <h1>Primeiro passo rumo aos 15k 😎</h1>
 }
 
-
+export default Home;
