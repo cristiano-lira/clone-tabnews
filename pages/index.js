89c6e0b2c047f2d6c1ac70e0e9017562e4e0a92d
Cristiano Lira <cristiano_sclira@hotmail.com>
2025-07-05 18:07:32 +0000

    adiciona o arquivo `.editorconfig` e remove o comentário no arquivo `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,5 @@
-// export default function Home() {
-//   return <h1>Primeiro passo rumo aos 15k 😎</h1>;
-// }
-
 function Home() {
-    return <h1>Primeiro passo rumo aos 15k 😎🤑 Se gostou, dê uma risadinha 😂</h1>
+  return <h1>Primeiro passo rumo aos 15k 😎🤑 Se gostou, dê uma risadinha 😂</h1>
 }
 
 export default Home;
